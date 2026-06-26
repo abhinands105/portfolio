cf5a1ce1adb453028f77c43ba01de4238514e852
Abhinand S <abhiske71@gmail.com>
2026-06-26 19:49:40 +0530

    Deploy latest build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react],
-
-  base: "/plane-portfolio/",
+  plugins: [react()],
+  base: "/portfolio/",
 });
