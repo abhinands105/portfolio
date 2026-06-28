90f48d7d71a88dd5dc2bfdf75514592aea0a7d22
Abhinand S <abhiske71@gmail.com>
2026-06-28 09:27:39 +0530

    Fix Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react],
-
-  server: {
-    watch: {
-      ignored: [
-        "**/*.zip",
-        "**/*.rar",
-        "**/*.7z"
-      ]
-    }
-  }
+  base: "/portfolio/",
+  plugins: [react()],
 });
